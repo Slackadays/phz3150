030ad2ade3d5bc5434e7875f18f101da9d6b2e83
Slackadays <lightingdzeyenr@gmail.com>
2023-09-02 15:41:11 -0400

    HW4

---
+++ Homeworks/hw4_jackson/acceleration.py
@@ -0,0 +1,5 @@
+def acceleration(u1: float, u2: float, t1: float, t2: float) -> float:
+        """Return the acceleration of an object given its initial velocity, final velocity, initial time, and final time."""
+        return (u2 - u1) / (t2 - t1)
+        # divide the change in velocity by the change in time to get the acceleration
+# note: I used type hints here to help ensure that logical stuff gets inputted
